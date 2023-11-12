7cd75f2fbf915452b52e6aaf91456a3415caab98
vgonisanz <vgonisanz@gmail.com>
2023-11-12 21:19:59 +0100

    core: Added list binary

---
+++ midicka/bin/list.py
@@ -0,0 +1,8 @@
+from midicka.core import Core
+
+def main():
+    core = Core()
+    core.list_midi_devices()
+
+if __name__ == "__main__":
+    main()
--- midicka/core.py
+++ midicka/core.py
@@ -1,12 +1,25 @@
 import mido
+import json
+
+
 from midicka.models import (
     KeyState,
     MidiMessage,
 )
 
+
 class Core:
-    def __init__(self, port_name: str):
+    def __init__(self, port_name: str = None):
         self.port_name = port_name
+
+    def list_midi_devices(self):
+        print("MIDI Input Devices:")
+        for input_name in mido.get_input_names():
+            print(f"'{input_name}'")
+
+        print("\nMIDI Output Devices:")
+        for output_name in mido.get_output_names():
+            print(f"'{output_name}'")
 
     def read(self):
         with mido.open_input(self.port_name) as inport:
@@ -21,3 +34,10 @@ class Core:
                         time=msg.time
                     )
                     yield msg_midi
+    
+    def write(self, midi_messages, file_path):
+        midi_data = [msg.model_dump() for msg in midi_messages]
+
+        with open(file_path, 'w') as file:
+            json.dump(midi_data, file, indent=4)
+
